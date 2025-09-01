12aed2dc7020d79ab2cfef36e2c6e0dbb771bb4c
Felipe Martins <felipe_martins__@outlook.com>
2025-09-01 15:18:51 -0300

    resolução simulado 3

---
+++ Exercicios_em_C/testes & estudos/Simulado 3/a1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char palavra[101];
+    scanf("%s", palavra);
+
+    int tamanho = strlen(palavra);
+
+    char inversao[101];
+    int j = 0;
+
+    for(int i = tamanho -1; i >= 0; i--) {
+        inversao[j] = palavra[i];
+        j++;
+    }
+
+    inversao[j] = '\0';
+
+    if(strcmp(palavra, inversao) == 0) {
+        printf("sim\n");
+    } else {
+        printf("nao\n");
+    }
+
+
+    return 0;
+}
