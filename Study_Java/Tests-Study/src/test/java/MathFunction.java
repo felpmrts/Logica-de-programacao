fd33bfff771c1764b3d739c7d8004e26830e9fb8
Felipe Martins <felipe_martins__@outlook.com>
2025-11-03 12:29:56 -0300

    teste

--- Study_Java/Tests-Study/src/test/java/MathFunction.java
+++ Study_Java/Tests-Study/src/test/java/MathFunction.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class MathFunction {
+
+    public static void main(String[] args) {
+
+    double x = 3.0;
+    double y = 4.0;
+    double z = -5.0;
+    double A, B, C;
+
+    System.out.printf("A raiz de %.0f eh: ", y);
+    A = Math.sqrt(y);
+    System.out.println(A);
+
+    System.out.printf("A potenciacao de %.0f elevado a 2 eh: ", x);
+    B = Math.pow(x, 2);
+    System.out.println(B);
+
+    System.out.printf("O valor absoluto de %.0f eh: ", z);
+    C = Math.abs(z);
+    System.out.println(C);
+
+
+    }
+
 }
--- Study_Java/Tests-Study/src/test/java/ScannerTest2.java
+++ Study_Java/Tests-Study/src/test/java/ScannerTest2.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ScannerTest2 {
+
+    public static void main(String[] args) {
+
+        Scanner primeiro = new Scanner(System.in);
+
+        String a, b, c;
+        int i;
+
+        i = primeiro.nextInt();
+        primeiro.nextLine(); // Consumir o \n -> limpar o buffer de leitura
+        a = primeiro.nextLine();
+        b = primeiro.nextLine();
+        c = primeiro.nextLine();
+
+        System.out.println("Dados digitados:");
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
 }
