0c840e0d0488a27b4ee5b625c9d30b1ab87b6c6b
Felipe Martins <felipe_martins__@outlook.com>
2025-11-09 18:38:24 -0300

    study++

--- Study_Java/Tests-Study/src/test/java/entities/Triangle.java
+++ Study_Java/Tests-Study/src/test/java/entities/Triangle.java
@@ -6,4 +6,12 @@ public class Triangle {
     public double b;
     public double c;
 
+    public double area() {
+
+        double p = (a + b + c) / 2.0;
+        double area = Math.sqrt(p * (p-a) * (p-b) * (p-c));
+
+        return area;
+    }
+
 }
